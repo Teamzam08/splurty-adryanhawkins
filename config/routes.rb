d507d8d396b0042f0a46c66ee50f2509dcc936ba
Adryan Hawkins <teamzaal@gmail.com>
2019-08-28 23:40:00 +0000

    Adding comment to test git

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For Hello world giot details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
